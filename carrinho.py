4baa7234c862f4388178254512710ad2cd35614e
Ronnie <121251352+roniepris@users.noreply.github.com>
2026-03-15 20:46:27 -0300

    Update test for calcular_total with new value

--- carrinho.py
+++ carrinho.py
@@ -1,7 +1,7 @@
 from carrinho import calcular_total, aplicar_desconto
 
 def test_calcular_total():
-    resultado = calcular_total(10, 3)
+    resultado = calcular_total(10, 4)
     assert resultado == 30
 
 
