35d6943d9534953a07956370b0eb30748b81d538
Ronnie <121251352+roniepris@users.noreply.github.com>
2026-03-15 20:20:48 -0300

    Create carrinho.py

---
+++ carrinho.py
@@ -0,0 +1,12 @@
+from carrinho import calcular_total, aplicar_desconto
+
+def test_calcular_total():
+    resultado = calcular_total(10, 3)
+    assert resultado == 30
+
+
+def test_aplicar_desconto():
+    total = 100
+    desconto = 0.1
+    resultado = aplicar_desconto(total, desconto)
+    assert resultado == 90
